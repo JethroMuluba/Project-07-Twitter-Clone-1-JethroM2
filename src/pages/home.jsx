389a926731a34b4882b6c58f616209d1381b0627
JethroMuluba <jethro.muluba@gmail.com>
2023-11-23 18:25:08 +0100

    Creat Header Component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Home() {
     return (
         <main className="timeline">
-            <h1>Timeline here</h1>
+            {/* <h1>Timeline here</h1> */}
+            <Header/>
         </main>
 
     );
