0daa69520b400e931ce5d9006c4e0bd9cef28440
JethroMuluba <jethro.muluba@gmail.com>
2023-11-23 20:00:16 +0100

    Creat the elements of Tweet Editor, avatar, input, icon actions and button

--- src/components/TweetEditor.jsx
+++ src/components/TweetEditor.jsx
@@ -1,7 +1,45 @@
+import avatar from '../images/profile-photo.png'
+import iconAction1 from '../Icons/Media.png'
+import iconAction2 from '../Icons/Gif.png'
+import iconAction3 from '../Icons/Poll.png'
+import iconAction4 from '../Icons/Emoji.png'
+import iconAction5 from '../Icons/Schedule.png'
+
+
+
+
+
 function TweetEditor () {
     return(
         <div className="tweet-editor">
+            <a href="#" className='avatar'>
+                <img src= {avatar} alt="User profil Avatar"/>
+            </a>
+            <div className='tweet-editor-form'>
+            <input type="text" placeholder=" What's happening? " className='tweet-editor-input'/>
+            <div className='tweet-editor-buttons'>
+                <article className='tweet-editor-actions'>
+                    <a href="#">
+                    <img src={iconAction1} alt="icon action" />
+                    </a>
+                    <a href="#">
+                    <img src={iconAction2} alt="icon action" />
+                    </a>
+                    <a href="#">
+                    <img src={iconAction3} alt="icon action" />
+                    </a>
+                    <a href="#">
+                    <img src={iconAction4} alt="icon action" />
+                    </a>
+                    <a href="#">
+                    <img src={iconAction5} alt="icon action" />
+                    </a>
+                </article>
+                <button type="button" className='button'>Tweet</button>
+            </div>
+            </div>
 
         </div>
     )
 }
+export default TweetEditor;
