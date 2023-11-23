bbe47cc2eb67cd5dbb04888f599a373a543b2a7e
JethroMuluba <jethro.muluba@gmail.com>
2023-11-23 18:43:46 +0100

    Creat Top Tweets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ function Header () {
     return(
         <div className="header">
             <h1 className="page-title">Home</h1>
+            <img src="src\Icons\Vector.png" alt="Top Tweets" className="top-tweets" />
 
         </div>
     )
