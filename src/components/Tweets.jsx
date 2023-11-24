f241f0feb05ea7f51e607883d59431a65a6ae289
JethroMuluba <jethro.muluba@gmail.com>
2023-11-24 15:34:47 +0100

    Creat my own Tweet

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -3,12 +3,15 @@ import avatarCnn from '../images/tweet-profile-photo.png';
 import avatarTnyt from '../images/Tweet-Profile-Photo-2.png';
 import tweetImage from '../images/tweet-image.png'
 import tweeterAvatar from '../images/Tweeter-Profile-Photo.png'
+import jethroTweet from '../images/Jethro-Tweet.png'
+import jethroProfil from '../images/Jethro-Profil.png'
 
 
 
 function Tweets () {
     return(
         <div className="tweets">
+            <Tweet tweetAvatar={jethroProfil} titleAuthor={"Jethro Muluba"} titleDetailAbbr={"@JethroMuluba"} titleDetailHourAndDate={'. Now'} tweetText={"As Web Designer / Ui and Ux. I'm a web designer with over five years of experience in creating a web mock-up andfunctional digital experience for many clients"} tweetImage={jethroTweet}/>
             <Tweet tweetAvatar={avatarCnn} titleAuthor={"CNN"} titleDetailAbbr={"@CNN"} titleDetailHourAndDate={'. 7m'} tweetText={'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.'}/>
             <Tweet tweetAvatar={avatarTnyt} titleAuthor={"The New York Times"} titleDetailAbbr={"@nytimes"} titleDetailHourAndDate={'. 2h'} tweetText={'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land'} tweetImage={tweetImage}/>
             <Tweet tweetAvatar={tweeterAvatar} titleAuthor={"Twitter"} titleDetailAbbr={"@Twitter"} titleDetailHourAndDate={'. Oct 4'} tweetText={'BIG NEWS lol jk still Twitter'} />
