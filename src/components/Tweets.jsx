71262fc4fef59b8f33f00895a11cad30ec1920bf
JethroMuluba <jethro.muluba@gmail.com>
2023-11-24 15:23:43 +0100

    Creat three Tweets sections

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -2,7 +2,8 @@ import Tweet from "./Tweet";
 import avatarCnn from '../images/tweet-profile-photo.png';
 import avatarTnyt from '../images/Tweet-Profile-Photo-2.png';
 import tweetImage from '../images/tweet-image.png'
-import jethroProfil from '../images/Jethro-Profil.png'
+import tweeterAvatar from '../images/Tweeter-Profile-Photo.png'
+
 
 
 function Tweets () {
@@ -10,6 +11,9 @@ function Tweets () {
         <div className="tweets">
             <Tweet tweetAvatar={avatarCnn} titleAuthor={"CNN"} titleDetailAbbr={"@CNN"} titleDetailHourAndDate={'. 7m'} tweetText={'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.'}/>
             <Tweet tweetAvatar={avatarTnyt} titleAuthor={"The New York Times"} titleDetailAbbr={"@nytimes"} titleDetailHourAndDate={'. 2h'} tweetText={'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land'} tweetImage={tweetImage}/>
+            <Tweet tweetAvatar={tweeterAvatar} titleAuthor={"Twitter"} titleDetailAbbr={"@Twitter"} titleDetailHourAndDate={'. Oct 4'} tweetText={'BIG NEWS lol jk still Twitter'} />
+            <Tweet tweetAvatar={tweeterAvatar} titleAuthor={"Twitter"} titleDetailAbbr={"@Twitter"} titleDetailHourAndDate={'. Oct 4'} tweetText={'hello literally everyone'} />
+            <Tweet tweetAvatar={tweeterAvatar} titleAuthor={"Twitter"} titleDetailAbbr={"@Twitter"} titleDetailHourAndDate={'. Oct 4'} tweetText={'hello literally everyone'} tweetImage={tweetImage} />
             
 
 
