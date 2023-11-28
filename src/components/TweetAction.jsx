6ccdc684f7486a488dea1429b66a06beed677185
JethroMuluba <jethro.muluba@gmail.com>
2023-11-28 01:43:38 +0100

    Use userState to increment and decrement the Tweet action

--- src/components/TweetAction.jsx
+++ src/components/TweetAction.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 function TweetAction ( {iconAction, counterAction} ) {
+    const [counter, setCounter] = useState( counterAction );
+    const [actionType, setActionType] = useState("increment");
+
+        const handleClick = () => {
+            if (actionType === "increment") {
+                setCounter(counter + 1);
+                setActionType("decrement");
+              } else {
+                setCounter(counter - 1);
+                setActionType("increment");
+              }
+        }
+           
      return(
         <div className="tweet-action">
-            <a href="#">
-            <img src= {iconAction}  alt="Action icon" />
-            </a>
-            <p> {counterAction} </p>
+            <button className='my-own-button' onClick={handleClick}>
+                <img src= {iconAction}  alt="Action icon" />
+            </button>
+            <p> {counter} </p>
 
         </div>
      )
