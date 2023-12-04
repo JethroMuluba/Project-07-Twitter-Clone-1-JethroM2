93581488ec929b0d08f0a60ca2555fdcda8790a9
JethroMuluba <jethro.muluba@gmail.com>
2023-12-04 19:28:07 +0100

    Delete import of Tweet component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import Header from '../components/Header';
 import TweetEditor from '../components/TweetEditor';
 import Tweets from '../components/Tweets';
-import Tweet from '../components/Tweet';
+
 
 function Home() {
     return (
         <main className="timeline">
-            
             <Header/>
             <TweetEditor/>
             <Tweets />
