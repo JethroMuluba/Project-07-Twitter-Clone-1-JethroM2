4b014eb9393dbed6dc021548a905849223409cf1
JethroMuluba <jethro.muluba@gmail.com>
2023-12-05 19:59:41 +0100

    Creat Sidebar Component and Import it to the Layaout Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 
 import('./style/reset.css')
 import('./style/App.css')
 export default function App() {
   return (
     <Layout>
+        <Sidebar/>
         <Home/>
     </Layout>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+function Sidebar() {
+    return(
+        <aside>
+            <h1>My Sidebar</h1>
+        </aside>
+    )
+}
+export default Sidebar;
