2ccafd6540b717deea9fd1a15b65e2fab8bd283d
JethroMuluba <jethro.muluba@gmail.com>
2023-12-08 00:08:12 +0100

    Style: fix the flexibility of Trends section

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import SettingsIcon from "../Icons/Settings.png";
 
 function Trends() {
   return (
     <div className= 'trends'>
         <div className='trends-search-bar-section'>
-           <input type="text" placeholder='search Twiiter' className='search-bar'/>
+            <input type="text" placeholder='search Twiiter' className='search-bar'/>
         </div>
-       
+        <div className='trends-for-you-section'>
+            <div className='trends-for-you-title'>
+                <p>Trends for you</p>
+                <button className='my-own-button'>
+                    <img src= {SettingsIcon} alt="settings icon" />
+                </button>
+
+            </div>
+
+        </div>
     </div>
   )
 }
