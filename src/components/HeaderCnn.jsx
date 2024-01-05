c8a614c2e65184abec9212af77c0ee6b62271c10
JethroMuluba <jethro.muluba@gmail.com>
2024-01-05 11:06:20 +0100

    feat: add user info in header of Cnn

--- src/components/HeaderCnn.jsx
+++ src/components/HeaderCnn.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import ArrowLeftIcon from "../Icons/arrow-back.svg";
+import iconVerified from "../Icons/Verified.png";
 
-export default function HeaderCnn() {
+
+
+export default function HeaderCnn({titleAuthor, titleAuthorAbbr}) {
     return (
-        <header className='header'>
+        <header>
 
             <button className="my-own-button">
-                {/* <img src= {ArrowLeftIcon} alt="Arrow left Icon" className='arrowLeftIcon'/> */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="white" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"></path><path fill="white" d="M6 8.5a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71L5.65.65c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L3.21 4.51l3.15 3.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"></path></svg>
             </button>
+
+            <div className='profile-settings-body'>
+                <p>
+                    <span className='tweet-title-author-set'> <a href="#" className='my-own-link-style' >{titleAuthor} <img src={iconVerified} alt="Icon Verified" /></a></span>
+                </p>
+
+                <p>
+                    <span className='tweet-title-details-set'> {titleAuthorAbbr} </span>
+                </p>
+            </div>
 
         </header>
     )
--- src/pages/CnnProfil.jsx
+++ src/pages/CnnProfil.jsx
@@ -3,9 +3,23 @@ import HeaderCnn from '../components/HeaderCnn';
 
 
 export default function CnnProfil() {
+  const userInfos = [
+    { 
+      id: 1,
+      name: "CNN",
+      postCounter: "420,7 k posts"
+    }
+  ]
   return (
     <main className="timeline">
-      <HeaderCnn/>
+      {userInfos.map((userInfo) => (
+      <HeaderCnn
+          key={userInfo.id}
+          titleAuthor={userInfo.name}
+          titleAuthorAbbr={userInfo.postCounter}
+      />
+      ))}
+      
       
         
     </main>
