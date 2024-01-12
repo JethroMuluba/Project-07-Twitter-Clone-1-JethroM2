15cf872b742d5bd2efc239e4a45f438df8f6d9e4
JethroMuluba <jethro.muluba@gmail.com>
2024-01-12 20:21:40 +0100

    Style : Install Tailwind and stylised Cnn Header

--- src/components/HeaderCnn.jsx
+++ src/components/HeaderCnn.jsx
@@ -8,12 +8,10 @@ export default function HeaderCnn({titleAuthor, titleAuthorAbbr}) {
     const navigate = useNavigate();
     const handleClick = (path) => {
         navigate(path);
-
-        console.log('HandleClick:', handleClick);
     }
 
     return (
-        <header className ="headerCnn">
+        <header className ="fixed w-[999px] flex gap-[55px] bg-black/75 border-b border-r border-px border-[#2F3336]  py-[20px] pl-[20px] backdrop-blur z-50 ">
 
             <button className="my-own-button" onClick={() => handleClick("/")}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="white" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"></path><path fill="white" d="M6 8.5a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71L5.65.65c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L3.21 4.51l3.15 3.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"></path></svg>
