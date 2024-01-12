8001ab21325af4cb4850600ed04e25bfaa85a592
JethroMuluba <jethro.muluba@gmail.com>
2024-01-12 21:08:12 +0100

    Style: stylised Home Header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header () {
     return(
       <header className="fixed w-[999px] flex justify-between items-center bg-black/60 border-b border-r border-px border-[#2F3336] px-[20px] py-[30px] backdrop-blur z-50 ">
-            <h1 className="page-title "> <a href="#" className="my-own-link-style">Home</a></h1>
+            <h1 className="text-2xl font-bold "> <span>Home</span></h1>
             <button className="my-own-button">
-                <img src="src\Icons\Vector.png" alt="Top Tweets" className="top-tweets" />
+                <img src="src\Icons\Vector.png" alt="Top Tweets" className="w-8 h-auto" />
             </button>
       </header>
     )
