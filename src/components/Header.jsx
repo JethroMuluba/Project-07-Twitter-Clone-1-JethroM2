64b7c9a931b1b5864ea0821a6d1b745d8aed5765
JethroMuluba <jethro.muluba@gmail.com>
2024-01-15 21:08:49 +0100

    Feat: make Home title navigable with useNavgate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 function Header () {
+  const navigate = useNavigate();
+  const handleClick = (path) => {
+      navigate(path);
+  }
+
+
     return(
       <header className="fixed w-[999px] flex justify-between items-center bg-black/60 border-b border-r border-px border-[#2F3336] px-[20px] py-[30px] backdrop-blur z-50 ">
-            <h1 className="text-2xl font-bold "> <span>Home</span></h1>
-            <button className="bg-none border-none ease-in-out cursor-pointer hover:bg-[#1D9BF0]/25 hover:rounded-full p-[10px]">
+
+            <button className="bg-none border-none cursor-pointer hover:rounded-full p-[10px]" onClick={() => handleClick("/")}>
+                <h1 className="text-2xl font-bold " > <span>Home</span></h1>
+            </button>
+
+            <button className="bg-none border-none cursor-pointer hover:bg-[#1D9BF0]/25 hover:rounded-full p-[10px]">
                 <img src="src\Icons\Vector.png" alt="Top Tweets" className="w-8 h-auto" />
             </button>
       </header>
