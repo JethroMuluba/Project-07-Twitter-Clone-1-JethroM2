abfee69bd329ab39049e6fb601dcaee601a923cc
JethroMuluba <jethro.muluba@gmail.com>
2024-01-16 23:38:21 +0100

    style: stylised of tweet button of sidebar

--- src/components/TweetButtonSidebar.jsx
+++ src/components/TweetButtonSidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function TweetButtonSidebar() {
   return (
     <div>
-        <button type="button" className='tweet-button-sidebar'>Tweet</button>
+        <button type="button" className='capitalize py-[1rem] px-[7rem] border-none rounded-full bg-[#1D9BF0] text-white text-[1rem] m-[0rem] cursor-pointer hover:bg-[#1a8fdc] '>Tweet</button>
     </div>
   )
 }
