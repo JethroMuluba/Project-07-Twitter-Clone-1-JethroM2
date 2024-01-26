b8eb29c13e4fdfe0a72f3a35bcd5aa5825a048a7
JethroMuluba <jethro.muluba@gmail.com>
2024-01-26 20:01:33 +0100

    Style: finished the style of user profil details

--- src/components/HeaderCnn.jsx
+++ src/components/HeaderCnn.jsx
@@ -11,7 +11,7 @@ export default function HeaderCnn({titleAuthor, titleAuthorAbbr}) {
     }
 
     return (
-        <header className ="fixed w-[881px] h-[100px] flex gap-[40px] bg-black/60 border-b  border-px border-[#2F3336]  py-[10px] pl-[20px] backdrop-blur z-50 ">
+        <header className ="fixed w-[881px] h-[100px] flex gap-[40px] bg-black/60 border-b  border-px border-[#2F3336]  py-[10px] pl-[20px] backdrop-blur z-50 pt-[10px] ">
 
             <button className="bg-none border-none ease-in-out cursor-pointer hover:bg-[#353535]/100 hover:rounded-full p-[10px]" onClick={() => handleClick("/")}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="1.78em" height="1em" viewBox="0 0 16 9"><path fill="white" d="M12.5 5h-9c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h9c.28 0 .5.22.5.5s-.22.5-.5.5"></path><path fill="white" d="M6 8.5a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71L5.65.65c.2-.2.51-.2.71 0c.2.2.2.51 0 .71L3.21 4.51l3.15 3.15c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"></path></svg>
@@ -31,7 +31,7 @@ export default function HeaderCnn({titleAuthor, titleAuthorAbbr}) {
                 </p>
 
                 <p>
-                    <span className='tweet-title-details-set'> {titleAuthorAbbr} </span>
+                    <span className='text-[1.2rem] font-[50px] text-[#6E767D] '> {titleAuthorAbbr} </span>
                 </p>
             </div>
 
--- src/components/UserProfilDetails.jsx
+++ src/components/UserProfilDetails.jsx
@@ -4,40 +4,51 @@ import iconVerified from "../Icons/Verified.png";
 
 export default function UserProfilDetails() {
   return (
-    <div className='user-profil-details-container'>
+    <div className='flex flex-col gap-[20px] mx-[20px] mt-[20px] '>
 
-        <div className='user-profil-detail-view'>
+        <div className='mt-[90px]'>
 
-        <div className='profile-settings-body'>
-          <p>
-            <span className='tweet-title-author-set'> <a href="#" className='my-own-link-style' > CNN <img src={iconVerified} alt="Icon Verified" /></a></span>
-          </p>
+        <div className='flex flex-col font-[Inter, sans-serif] '>
+            <p>
+                <span> 
+                    <span className='flex place-items-center gap-[2px]'>
+                        <span  className="text-[30px] font-bold " >
+                            CNN
+                        </span>
+                        <img src={iconVerified} alt="Icon Verified" className='w-[30px] h-[30px]  '/>
+                    </span>
+                </span>
+            </p>
 
-          <p>
-            <span className='tweet-title-details-set'> @CNN </span>
-          </p>
+            <p>
+              <span className='text-[1.2rem] font-[50px] text-[#6E767D] '> @CNN </span>
+            </p>
 
         </div>
 
         </div>
 
 
-        <p className='user-text-purpos'>
-          It's our job to #GoThere & tell the most difficul stories. For breaking news, follow @CNNBRK and download our app cnn.com/apps
+        <p className='text-[22px] '>
+          It's our job to <a href='#'className='hover:underline hover:decoration-[#1D9BF0]'> <span className='text-[#1D9BF0] '>#GoThere</span></a> & tell the most difficul stories. For breaking news, follow  <a href='#' className='hover:underline hover:decoration-[#1D9BF0]'> <span className='text-[#1D9BF0] '>@CNNBRK</span></a> and download our app <a href='#'className='hover:underline hover:decoration-[#1D9BF0]'> <span className='text-[#1D9BF0] '>cnn.com/apps</span></a> 
         </p>
 
-        <div className='user-elements'>
+        <div className='flex gap-[20px] '>
 
-          <div className='link-elements'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.615 16.077H7.077q-1.692 0-2.884-1.192Q3 13.693 3 12q0-1.691 1.193-2.885q1.192-1.193 2.884-1.193h3.538v1H7.077q-1.27 0-2.173.904T4 12q0 1.27.904 2.173t2.173.904h3.538zM8.5 12.5v-1h7v1zm4.885 3.577v-1h3.538q1.27 0 2.173-.904T20 12q0-1.27-.904-2.173t-2.173-.904h-3.538v-1h3.538q1.692 0 2.885 1.192Q21 10.307 21 12q0 1.691-1.193 2.885q-1.192 1.193-2.884 1.193z"></path></svg>
-            <p>
-              cnn.com
+          <div className='flex gap-[10px]'>
+            <svg xmlns="http://www.w3.org/2000/svg" className='w-[30px] h-[30px] mb-[4px] mt-[5px] cursor-pointer'  width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10.615 16.077H7.077q-1.692 0-2.884-1.192Q3 13.693 3 12q0-1.691 1.193-2.885q1.192-1.193 2.884-1.193h3.538v1H7.077q-1.27 0-2.173.904T4 12q0 1.27.904 2.173t2.173.904h3.538zM8.5 12.5v-1h7v1zm4.885 3.577v-1h3.538q1.27 0 2.173-.904T20 12q0-1.27-.904-2.173t-2.173-.904h-3.538v-1h3.538q1.692 0 2.885 1.192Q21 10.307 21 12q0 1.691-1.193 2.885q-1.192 1.193-2.884 1.193z"></path></svg>
+            <p className='text-[22px] '>
+              <a href='#' className='hover:underline hover:decoration-[#1D9BF0]'> 
+                <span className='text-[#1D9BF0] '>
+                  cnn.com
+                </span>
+              </a>
             </p>
           </div>
 
-          <div className='link-elements'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="gray" d="M12 14a1 1 0 1 0-1-1a1 1 0 0 0 1 1m5 0a1 1 0 1 0-1-1a1 1 0 0 0 1 1m-5 4a1 1 0 1 0-1-1a1 1 0 0 0 1 1m5 0a1 1 0 1 0-1-1a1 1 0 0 0 1 1M7 14a1 1 0 1 0-1-1a1 1 0 0 0 1 1M19 4h-1V3a1 1 0 0 0-2 0v1H8V3a1 1 0 0 0-2 0v1H5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3m1 15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9h16Zm0-11H4V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1ZM7 18a1 1 0 1 0-1-1a1 1 0 0 0 1 1"></path></svg>
-            <p>
+          <div className='flex gap-[10px] '>
+          <svg xmlns="http://www.w3.org/2000/svg" className='w-[25px] h-[25px] mb-[4px] mt-[5px] cursor-pointer' width="1em" height="1em" viewBox="0 0 24 24"><path fill="gray" d="M12 14a1 1 0 1 0-1-1a1 1 0 0 0 1 1m5 0a1 1 0 1 0-1-1a1 1 0 0 0 1 1m-5 4a1 1 0 1 0-1-1a1 1 0 0 0 1 1m5 0a1 1 0 1 0-1-1a1 1 0 0 0 1 1M7 14a1 1 0 1 0-1-1a1 1 0 0 0 1 1M19 4h-1V3a1 1 0 0 0-2 0v1H8V3a1 1 0 0 0-2 0v1H5a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3m1 15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9h16Zm0-11H4V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1ZM7 18a1 1 0 1 0-1-1a1 1 0 0 0 1 1"></path></svg>
+            <p className='text-[22px] text-[#6E767D]'>
               Join Tweeter on febuary 2007
             </p>
           </div>
