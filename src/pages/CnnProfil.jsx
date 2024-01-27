4f9f0ff1752c452746c7192a3aeb87b62c020d76
JethroMuluba <jethro.muluba@gmail.com>
2024-01-27 01:07:34 +0100

    Feat: add tweet of Cnn profile

--- src/pages/CnnProfil.jsx
+++ src/pages/CnnProfil.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import HeaderCnn from '../components/HeaderCnn';
 import UserProfilTweet from '../components/UserProfilTweet';
 import UserProfilDetails from '../components/UserProfilDetails';
-import Tweets from '../components/Tweets';
 import TimelineNav from '../components/TimelineNav';
+import avatarCnn from '../images/tweet-profile-photo.png';
+import Tweet from '../components/Tweet';
 
 
 export default function CnnProfil() {
@@ -15,6 +16,21 @@ export default function CnnProfil() {
       postCounter: "420,7 k posts"
     }
   ];
+
+
+  const cnnData = [
+    {
+      id: 1,
+      tweetAvatar: avatarCnn,
+      titleAuthor: "CNN", 
+      titleDetailAbbr: "@CNN",
+      titleDetailHourAndDate: ". 7m",
+      tweetText: 'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.', 
+      tweetActionReply: 57, 
+      tweetActionRetweet: 144,
+      tweetActionReact: 184,
+  },
+  ]
 
   return (
 
@@ -29,8 +45,26 @@ export default function CnnProfil() {
       ))}
 
       <UserProfilTweet/>
+
       <UserProfilDetails/>
+
       <TimelineNav/>
+
+      {cnnData.map((tweet) => (
+      <Tweet
+            key={tweet.id}
+            tweetAvatar={tweet.tweetAvatar}
+            titleAuthor={tweet.titleAuthor}
+            titleDetailAbbr={tweet.titleDetailAbbr}
+            titleDetailHourAndDate={tweet.titleDetailHourAndDate}
+            tweetText={tweet.tweetText}
+            tweetImage={tweet.tweetImage}
+            tweetActionReply={tweet.tweetActionReply}
+            tweetActionRetweet={tweet.tweetActionRetweet}
+            tweetActionReact={tweet.tweetActionReact}
+        />
+        ))}
+    
       
 
       
